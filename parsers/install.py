aa7b09ef20ce46a57e8dff560b0ce3b98e6e5698
Kamil Rykowski <kamil.rykowski@gmail.com>
2017-02-03 11:06:21 +0100

    Base version of the jokes parser

---
+++ parsers/install.py
@@ -0,0 +1,155 @@
+import os
+import shutil
+from subprocess import call
+
+import click
+from lxml import etree
+from user_agent import generate_user_agent
+from pyquery import PyQuery as pq
+import requests
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def parse():
+    src_dir = '.'
+    app_dir = '..'
+
+    base_url = 'http://perelki.net/'
+    response = requests.get(base_url, headers={'User-Agent': generate_user_agent()})
+    btree = etree.HTML(response.content)
+
+    first_page = 1
+    last_page = 2 # int(btree.xpath('/html/body/div[6]/a[last()]')[0].text)
+
+    jokes = []
+    for page_nr in range(first_page, last_page + 1):
+        page_url = base_url + '?ps={}'.format(page_nr)
+        page_response = requests.get(page_url, headers={'User-Agent': generate_user_agent()})
+        ptree = etree.HTML(page_response.content)
+
+        nodes = ptree.xpath('/html/body/div[5]/div[@class="container" and @class!="ad"]')
+
+        for node in nodes:
+            meta_node = node.find('div[@class="about"]')
+
+            # Many elements on the page has container class. Skip them
+            # if no joke metadata available
+            if not meta_node:
+                continue
+
+            node.remove(meta_node)
+
+            pq_detail = pq(etree.tostring(node, encoding='unicode'))
+            pq_meta = pq(etree.tostring(meta_node, encoding='unicode'))
+
+            joke_text = (
+                pq_detail.html().
+                    replace('<br/>', '\n').
+                    replace('&#13;', '').
+                    strip()
+            )
+            joke_id = pq_meta.find('a')[1].text.strip()
+            joke_rate = int(pq_meta.find('span')[1].text.strip())
+            joke_date = pq_meta.find('span')[2].text.strip()
+
+            try:
+                # Some jokes don't have any author
+                joke_author = pq_meta.find('span')[3].text.strip()
+            except IndexError:
+                joke_author = None
+
+            joke = {
+                'id': joke_id,
+                'content': joke_text,
+                'rate': joke_rate,
+                'date': joke_date,
+                'author': joke_author
+            }
+            jokes.append(joke)
+
+    print(jokes)
+
+    # Copy parsed file
+    # icon_src = '{}/static/icon.png'.format(src_dir)
+    # icon_dest = '{}/src'.format(app_dir)
+
+    # shutil.copy(icon_src, icon_dest)
+
+
+@cli.command()
+def clean():
+    app_dir = '..'
+
+    os.chdir(app_dir)
+
+    # Clear www directory
+    shutil.rmtree("www", ignore_errors=True)
+    os.makedirs("www")
+
+    shutil.rmtree("platforms", ignore_errors=True)
+    shutil.rmtree("plugins", ignore_errors=True)
+    call(["ionic", "state", "restore"])
+
+
+@cli.command()
+def build():
+    provider = 'jokes_app'
+    app_dir = '..'
+
+    os.chdir(app_dir)
+
+    call(["ionic", "resources"])
+
+    # Build android package
+    call(["ionic", "build", "android", "--release", "--prod"])
+
+    def jarsigner(apk_name):
+        call([
+            "jarsigner", "-verbose",
+            "-sigalg", "SHA1withRSA",
+            "-digestalg", "SHA1",
+            "-keystore", "puppybox-release.keystore",
+            "platforms/android/build/outputs/apk/{}".format(apk_name),
+            "puppybox-release"
+        ])
+
+    def zipalign(src_apk, dest_apk):
+        try:
+            os.remove(dest_apk)
+        except:
+            pass
+
+        call([
+            "zipalign",
+            "-v", "4",
+            "platforms/android/build/outputs/apk/{}".format(src_apk),
+            "{}".format(dest_apk)
+        ])
+
+    src_apk = "android-armv7-release-unsigned.apk"
+    jarsigner(src_apk)
+    zipalign(src_apk, "builds/jokes_app_arm.apk".format(provider))
+
+    src_apk = "android-x86-release-unsigned.apk"
+    jarsigner(src_apk)
+    zipalign(src_apk, "builds/{}_x86.apk".format(provider))
+
+    # Build ios package, it's strange but I have to remove/add the platform
+    call(["ionic", "platform", "rm", "ios"])
+    call(["ionic", "platform", "add", "ios"])
+    call(["ionic", "build", "ios"])
+
+
+@cli.command()
+def deploy():
+    parse()
+    build()
+
+
+if __name__ == '__main__':
+    cli()
